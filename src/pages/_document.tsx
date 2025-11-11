1905ccf6de4b62710a9f38562a9e7defb0058edb
Adam Young <adamyoungy678@gmail.com>
2025-11-11 01:44:55 +0000

    multi emailgp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CXLD6HDGHJ"
+        ></Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CXLD6HDGHJ');
+            `,
+          }}
+        />
+      </Head>
       <body className="bg-zinc-950 text-zinc-200 selection:bg-zinc-600">
         <Main />
         <NextScript />
